27ac01a8322a7d1875f814c4c770796d9f2cac99
kof-f <kofif7kofi@gmail.com>
2020-01-09 21:50:55 -0500

    Cleaned up tip calculator

--- tip-calculator/tip-calculator.js
+++ tip-calculator/tip-calculator.js
@@ -1,9 +1,12 @@
+"use strict";
+
+let tipForm = document.getElementById("wf-form-Tip-Form");
+
 // initializing variables
 let tipAmount;
 let tipAmountPerGuest;
 
 function tipCalculator(numOfGuests, totalAmountPaid, percentOfTip){
-    
     /*** CALCULATE PERCENTAGE
      * Multiply the number by the percent (e.g. 87 * 68 = 5916)
      * Divide the answer by 100 (Move decimal point two places to the left) (e.g. 5916/100 = 59.16)
@@ -16,21 +19,29 @@ function tipCalculator(numOfGuests, totalAmountPaid, percentOfTip){
 
     // using the percentage formula to calculate the tip based on the tip percent entered
     tipAmount = (totalAmountPaid * percentOfTip) / 100;
+    tipAmount = Math.round(100*tipAmount)/100;
 
     // calculate the amount each guest should tip
     tipAmountPerGuest = tipAmount / numOfGuests;
+    tipAmountPerGuest = Math.round(100*tipAmountPerGuest)/100; 
 }
 
 function display(){
-
     let tipInformation = ["The total amount you should tip is: $" + tipAmount, 
     "Each guest should tip: $" + tipAmountPerGuest].join("\n");
 
-    console.log(tipInformation);
-            
-    // copying time to the div (using inner text and text content to support Firefox and IE
-    document.getElementById(".clock-section").innerText = "Your tip information: " + tipInformation;
-    document.getElementById(".clock-section").textContent = "Your tip information: " + tipInformation;
+    // copying new title to the form heading
+    document.getElementById("form-heading").innerText = "Your tip information: ";
+    document.getElementById("form-heading").textContent = "Your tip information: ";
+    
+    // styling the success msg using pre-defined classes
+    let successMsg = document.getElementById("success-msg");
+    successMsg.classList.add("title-paragraph");
+    successMsg.style.whiteSpace = "pre";
+    
+    // copying tio information to the success msg
+    successMsg.innerText = tipInformation;
+    successMsg.textContent = tipInformation;
 }
 
 function startDisplay(){
@@ -44,3 +55,4 @@ function startDisplay(){
     // display the tip on the web page
     display();
 }
+tipForm.addEventListener('submit', startDisplay);
