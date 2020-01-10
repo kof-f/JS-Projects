7890b3b125c06a56657e0c5d4efa2aa59e0e9660
kof-f <kofif7kofi@gmail.com>
2020-01-09 20:05:28 -0500

    Cleaned up countdown clock

--- countdown-clock/countdown.js
+++ countdown-clock/countdown.js
@@ -1,57 +1,64 @@
+"use strict";
+
+let countdownForm = document.getElementById("wf-form-Countdown-Form");
+
 // initializing variables
 let sec;
 let min;
 let hour;
 let day;
 
-function countdown(){
-    
-    // initailizing to be used for the current time
-    let now = new Date();
+    function countdown(){
+        // initailizing to be used for the current time
+        let now = new Date();
 
-    // taking input from the form to create event date object
-    let eventDateData = document.getElementById("eventDate").value;
-    let eventDate = new Date(eventDateData);
-    
-    // convert the dates to millseconds to find the difference between the two dates
-    let currentTime = now.getTime();
-    let eventTime = eventDate.getTime();
+        // taking input from the form to create event date object
+        let eventDateData = document.getElementById("eventDate").value;
+        let eventDate = new Date(eventDateData);
+        
+        // convert the dates to millseconds to find the difference between the two dates
+        let currentTime = now.getTime();
+        let eventTime = eventDate.getTime();
 
-    let remTime = eventTime - currentTime;
+        let remTime = eventTime - currentTime;
 
-    // converting millseconds to seconds, minutes, hours and days
-    sec = Math.floor(remTime / 1000);
-    min = Math.floor(sec / 60);
-    hour = Math.floor(min / 60);
-    day = Math.floor(hour / 24);
+        // converting milliseconds to seconds, minutes, hours and days
+        sec = Math.floor(remTime / 1000);
+        min = Math.floor(sec / 60);
+        hour = Math.floor(min / 60);
+        day = Math.floor(hour / 24);
 
-    hour %= 24;
-    min %= 60;
-    sec %= 60;
+        hour %= 24;
+        min %= 60;
+        sec %= 60;
 
-}
-
-function display(eventName){
-    
-    // set eventName to "event" if it's not set
-    if (eventName == ""){
-        eventName = "event";
     }
 
-    let timeRemaining = [day + " days", 
-    hour + " hours",
-    min + " minutes",
-    sec + " seconds."].join("\n");
+    function display(eventName, h, m, s){
+        // set eventName to "event" if it's not set
+        if (eventName == ""){
+            eventName = "event";
+        }
 
-    console.log(timeRemaining);
-            
-    // copying time to the div (using inner text and text content to support Firefox and IE
-    document.getElementById(".clock-section").innerText = "Countdown to " + eventName + ": " + timeRemaining;
-    document.getElementById(".clock-section").textContent = "Countdown to " + eventName + ": " + timeRemaining; 
-}
+        let timeRemaining = [day + " days", 
+        hour + " hours",
+        min + " minutes",
+        sec + " seconds"].join("\n");
+                
+        // copying the event name to the form's heading
+        document.getElementById("form-heading").innerText = "countdown to " + eventName;
+        document.getElementById("form-heading").textContent = "Countdown to " + eventName;
+        
+        // adding styling to the success message and copying the time to it
+        let successMsg = document.getElementById("success-msg");
+        successMsg.style.whiteSpace = "pre";
+        successMsg.classList.add("countdown-clock-num");
+        successMsg.innerText = timeRemaining;
+        successMsg.textContent = timeRemaining; 
+    }
 
-function startDisplay(){
-    let eventNameData = document.getElementById("eventName").value;
-    setInterval( function(){ countdown(); display(eventNameData); }, 1000);
-
-}
+    function startDisplay(){
+        let eventNameData = document.getElementById("eventName").value;
+        setInterval( function(){ countdown(); display(eventNameData, hour, min, sec); }, 1000);
+    }
+countdownForm.addEventListener('submit', startDisplay);
